023417c11a8912e404e60695ff3e403bd369d3e2
Alexey Dubovko <adubovko@gmail.com>
2020-09-10 18:07:42 +0500

    ДЗ для 7 урока (начало)

--- index.php
+++ index.php
@@ -1,66 +1,41 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $number1 = $_POST['number1'];
-    $number2 = $_POST['number2'];
-    $operation = $_POST['operation'];
-    $reset = $_POST['reset'];
-
-    if ($reset) {
-        $view = "";
-        $number1 = 0;
-        $number2 = 0;
-        $operation = false;
-        $view = "";
-    }
-
-    if ($operation) {
-        if ($operation == "+") {
-            $result = $number1 + $number2;
-        }
-        if ($operation == "-") {
-            $result = $number1 - $number2;
-        }
-        if ($operation == "*") {
-            $result = $number1 * $number2;
-        }
-        if ($operation == "/") {
-            $result = ($number2 != 0) ? $number1 / $number2 : "Нельзя делить на 0";
-        }
-    }
-}
+include_once "models/db_goods.php";
 ?>
 
 <!DOCTYPE html>
-<html lang="ru">
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Калькулятор</title>
+<meta charset="utf-8">
+<title>Интернет-магазин ноутбуков</title>
+<link rel="stylesheet" href="css/styles.css" type="text/css" media="all">
 </head>
-<body>
-<div style="width:70%; margin:0 auto; padding-top: 10px; text-align: center;">
-<h2>Калькулятор</h2>
-<form action="" method="post">
-    <input name="number1" type="text" value="<?= $number1 ?>">
-    <?= $operation ?>
-    <input name="number2" type="text" value="<?= $number2 ?>">
-    = <?= $result ?><br>
-    <h4>Через select</h4>
-    <select name="operation" onchange="submit()">
-        <option value="">Операция</option>
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
-    </select><br>
-    <h4>Через кнопки</h4>
-    <input name="operation" value="+" type="submit"/>
-    <input name="operation" value="-" type="submit"/>
-    <input name="operation" value="*" type="submit"/>
-    <input name="operation" value="/" type="submit"/><br>
-    <h4>Сброс</h4>
-    <input name="reset" value="Reset" type="submit"/>
-</form>
+<body> 
+<div id="container">
+    <?= include "templates/header.php"; ?>
+    <div class="leftblock">
+        <?= include "templates/menu.php"; ?>
+    </div>
+    <div class="content">
+        <h1>Интернет-магазин ноутбуков</h1>
+        <p class="hello">Добро пожаловать в наш интернет-магазин ноутбуков, у нас огромный ассортимент комплектующих и расходных материалов для ремонта ноутбуков, планшетов и телефонов. </p>
+        <hr>
+        <?php
+        $goods = goods_all($link);
+        if($goods){
+            foreach ($goods as $good){?>
+                <div class="item">
+                    <a href="item.php?id=<?=$good['id']?>"><img src="<?=$good['small_src']?>" alt="<?=$good['name']?>" title="<?=$good['name']?>"></a>
+                    <h3 class="item-name"><a href="item.php?id=<?=$good['id']?>"><?=$good['name']?></a></h3>
+                    <p class="price"><?=$good['price']?>р.</p>
+                    <p class="add-to-basket"><a href="#" title="Добавить в корзину">Купить</a></p>
+                </div>
+            <?}
+        }
+        ?>
+    </div>
+    <footer>
+        <?= include "templates/footer.php"; ?>
+    </footer>
 </div>
 </body>
 </html>
